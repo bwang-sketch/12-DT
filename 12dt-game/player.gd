d583c97508c74c026247a6fc02bdb810083869af
bwang-sketch <bwang@students.mags.school.nz>
2026-06-08 13:09:05 +1200

    made basic movement

---
+++ 12dt-game/player.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+var speed: float = 300.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var direction: Vector2 = Vector2(0.0, 0.0)
+	direction.x = Input.get_axis("ui_left", "ui_right")
+	direction.y = Input.get_axis("ui_up", "ui_down")
+
+	velocity = speed * direction.normalized()
+	move_and_slide()
