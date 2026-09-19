59e320761cd270b2cdeddd44b1626c280260a9eb
bwang-sketch <bwang@students.mags.school.nz>
2026-09-19 21:34:45 +1200

    projectile_slash_ani

--- 12dt-game/scripts/projectile_slash.gd
+++ 12dt-game/scripts/projectile_slash.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
-var speed: float = 1300.0
+var speed: float = 2000.0
+
+@export var animated_sprite: AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,6 +10,7 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	move_local_x(speed * delta)
+	animated_sprite.play("projectile_slash_ani")
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("enemies"):
